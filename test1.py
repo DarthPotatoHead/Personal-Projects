eaf922f8b5a27623a6b1c2a66c18115fa879426d
ainsley <ainsleyb2007@gmail.com>
2025-01-22 15:59:10 -0600

    first commit to push test.py to remote

---
+++ test1.py
@@ -0,0 +1,6 @@
+haystack = ['hay','junk','hay','hay', 'moreJunk','needle','randomJunk']
+def findneedle(haystack):
+    index = haystack.index('needle')
+    if 'needle' in haystack:
+        print('found the needle at position ', index)
+findneedle(haystack)
